a3c95a5555859eba3228344a0ad71563982d41e3
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-08 08:48:46 +0200

    Primer ejercicio del boletin 1

---
+++ boletin1_1/Boletin1_1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin1_1;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class Boletin1_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int base = 4, altura = 3, area;
+        area = (base*altura)/2;
+        System.out.println("area = "+area);
+    }
+    
+}
